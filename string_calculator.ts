974796b99c93c2a2cf828b4fb986dfcea81e69eb
Wolfgang Wieser <w.wies3r@gmail.com>
2019-02-08 09:34:50 +0100

    StringCalculator: add support for splitting by several separators.

--- string_calculator.ts
+++ string_calculator.ts
@@ -1,12 +1,36 @@
 export class StringCalculator {
-    static Add(input: string ): number {
+
+    private numberStrings: string[] = [];
+    private separators: string[] = [','];
+
+    constructor() {
+
+    }
+
+    private splitNumberStrings(separatorIndex: number) {
+      let newNumberStrings: string[] = [];
+      const currentSeparator = this.separators[separatorIndex];
+      this.numberStrings.forEach((numberString) => {
+          newNumberStrings = newNumberStrings.concat(numberString.split(currentSeparator));
+      });
+      this.numberStrings = newNumberStrings;
+    }
+
+    private reset() {
+      this.numberStrings = [];
+    }
+
+    Add(input: string ): number {
+      this.reset();
+
       if(!input)
         return 0;
 
-      const numbers = input.split(",");
+      this.numberStrings = [input];
+      this.splitNumberStrings(0);
       let sum = 0;
 
-      numbers.forEach((value) => sum += parseInt(value));
+      this.numberStrings.forEach((value) => sum += parseInt(value));
 
       return sum;
     }
--- tests/string_calculator.test.ts
+++ tests/string_calculator.test.ts
@@ -3,19 +3,22 @@ import { expect } from 'chai';
 import { StringCalculator } from '../string_calculator';
 
 describe('StringCalculator', function() {
+
+  const stringCalculator = new StringCalculator();
+
   it('empty input', function() {
-    expect(StringCalculator.Add('')).to.be.equal(0);
+    expect(stringCalculator.Add('')).to.be.equal(0);
   });
 
   it('single number input', function() {
-    expect(StringCalculator.Add('1')).to.be.equal(1);
-    expect(StringCalculator.Add('121')).to.be.equal(121);
-    expect(StringCalculator.Add('12321')).to.be.equal(12321);
+    expect(stringCalculator.Add('1')).to.be.equal(1);
+    expect(stringCalculator.Add('121')).to.be.equal(121);
+    expect(stringCalculator.Add('12321')).to.be.equal(12321);
   });
 
   it('simple sequence of numbers', function() {
-    expect(StringCalculator.Add('1,1')).to.be.equal(2);
-    expect(StringCalculator.Add('1,2,1')).to.be.equal(4);
-    expect(StringCalculator.Add('1,2,3,2,1')).to.be.equal(9);
+    expect(stringCalculator.Add('1,1')).to.be.equal(2);
+    expect(stringCalculator.Add('1,2,1')).to.be.equal(4);
+    expect(stringCalculator.Add('1,2,3,2,1')).to.be.equal(9);
   });
 });
